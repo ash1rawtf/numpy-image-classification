4516b02a8c0ebbdf8a002e1b691ce6d30abc277e
ash1ra <ash1rawtf@gmail.com>
2025-01-21 13:20:33 +0200

    feat: add init_params() function
    feat: add ReLU() and softmax() functions
    
    feat: add forward_prop() function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy.typing import NDArray
 
 TEST_SAMPLES_COUNT = 1000
+LEARNING_RATE = 0.1
 
 
 def get_data() -> tuple[NDArray[np.int64]]:
@@ -21,9 +22,41 @@ def get_data() -> tuple[NDArray[np.int64]]:
     return X_train, Y_train, X_test, Y_test
 
 
+def init_params() -> tuple[NDArray[np.float64]]:
+    print(np.random.rand(10, 10) - 0.5)
+    W1 = (np.random.rand(10, 784) - 0.5).astype("float128")
+    b1 = (np.random.rand(10, 1) - 0.5).astype("float128")
+
+    W2 = (np.random.rand(10, 10) - 0.5).astype("float128")
+    b2 = (np.random.rand(10, 1) - 0.5).astype("float128")
+
+    return W1, b1, W2, b2
+
+
+def ReLU(Z) -> NDArray[np.float64]:
+    return np.maximum(0, Z)
+
+
+def softmax(Z) -> NDArray[np.float64]:
+    return np.exp(Z) / np.sum(np.exp(Z), axis=0)
+
+
+def forward_prop(X, W1, b1, W2, b2) -> None:
+    Z1 = np.dot(W1, X) + b1
+    A1 = ReLU(Z1)
+
+    Z2 = np.dot(W2, A1) + b2
+    A2 = softmax(Z2)
+
+    return A2
+
+
 def main() -> None:
     X_train, Y_train, X_test, Y_test = get_data()
-    print(Y_train.dtype)
+    W1, b1, W2, b2 = init_params()
+
+    A2 = forward_prop(X_train, W1, b1, W2, b2)
+    print(A2)
 
 
 if __name__ == "__main__":
